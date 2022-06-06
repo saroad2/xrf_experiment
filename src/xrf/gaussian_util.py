12f60efd06494623ae7e00b6f08d4030a349fb2f
Sagi Shadur <saroad2@gmail.com>
2022-06-06 10:15:38 +0300

    Use scipy.find_peaks to find peaks

--- src/xrf/gaussian_util.py
+++ src/xrf/gaussian_util.py
@@ -2,19 +2,13 @@ from typing import List
 
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy.signal import find_peaks
 
 from xrf.constants import EPSILON
 
 
 def extract_local_maxima_indices(x: np.ndarray, n: int):
-    max_indices = []
-    for i in range(n + 1, x.shape[0] - n):
-        max_candidate = x[i]
-        neighbourhood = np.concatenate([x[i - n : i], x[i + 1 : i + n + 1]])
-        neighbourhood_max = neighbourhood.max()
-        if max_candidate >= neighbourhood_max >= EPSILON:
-            max_indices.append(i)
-    return max_indices
+    return find_peaks(x, width=n, height=EPSILON)[0]
 
 
 def trim_gaussian(x: np.ndarray, y: np.ndarray, start_x: List[int]):
